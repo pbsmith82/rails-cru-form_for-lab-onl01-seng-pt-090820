23334417f5f0d2d1eccbcb55c8a1396e91a2cf88
pbsmith82 <pbsmith82@gmail.com>
2021-01-03 19:12:15 -0500

    commit tree 1a2cd2afcc43a6ec3c15750b0de018620b817f97

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :genres, only: [:new, :update, :edit, :show, :create]
+  resources :songs, only: [:index, :new, :update, :edit, :show, :create]
+  resources :artists, only: [:new, :update, :edit, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  patch 'genres/:id', to: 'genres#update'
+  patch 'songs/:id', to: 'songs#update'
+  patch 'artists/:id', to: 'artists#update'
+
 end
